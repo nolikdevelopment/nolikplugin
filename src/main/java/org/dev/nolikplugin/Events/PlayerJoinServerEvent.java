f303d5b7e3be57f758fa9efcc4c6ce2ceca39968
nolikdevelopment <knik7536@yandex.ru>
2025-09-02 23:28:26 +0300

    апдейт евента

--- src/main/java/org/dev/nolikplugin/Events/PlayerJoinServerEvent.java
+++ src/main/java/org/dev/nolikplugin/Events/PlayerJoinServerEvent.java
@@ -8,11 +8,18 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerJoinServerEvent implements Listener {
+    // TODO: СДЕЛАТЬ ЧТО - ТО ПОХОЖЕЕ НА НАБОР НОВИЧКА, ДА МНЕ СЕЙЧАС ЛЕНЬ
     @EventHandler
     private void onMessage(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        event.setJoinMessage("Игрок " + player.getName() + " подключился к серверу.");
-        ItemStack itemStack = new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 1);
-        player.getInventory().addItem(itemStack);
+        if (!player.hasPlayedBefore()) {
+            event.setJoinMessage("Игрок " + player.getName() + " впервые попал на сервер!");
+            ItemStack itemStack = new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 1);
+            player.getInventory().addItem(itemStack);
+        } else {
+            event.setJoinMessage("Игрок " + player.getName() + " подключился к серверу.");
+            ItemStack itemStack = new ItemStack(Material.AIR);
+            player.getInventory().addItem(itemStack);
+        }
     }
 }
